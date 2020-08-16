3b300001863f7a66e07b686ca357f7ce54eab9bd
Martin Mena <mmena@stackbuilders.com>
2020-08-15 19:12:06 -0500

    Add translation feature

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import argparse
 import sys
 
 from core.thesaurus import list_definitions
+from core.translate import translate
 
 def parse_args():
     # Create the parser
@@ -18,6 +19,26 @@ def parse_args():
         type=str,
         help='list the definitions of the given word'
     )
+    my_parser.add_argument(
+        '-t',
+        '--translate',
+        metavar='text',
+        nargs='?',
+        type=str,
+        help='translates to Spanish the provided English text'
+    )
+    my_parser.add_argument(
+        '--from',
+        metavar='language',
+        type=str,
+        help='uses the suplied language as source'
+    )
+    my_parser.add_argument(
+        '--to',
+        metavar='language',
+        type=str,
+        help='uses the suplied language as destination'
+    )
     return my_parser.parse_args()
 
 if __name__ == "__main__":
@@ -29,6 +50,18 @@ if __name__ == "__main__":
         )
         if text == '1':
             list_definitions()
+        elif text == '2':
+            translate()
+        else:
+            print("Invalid option.")
+            sys.exit(2)
     args = parse_args()
     if args.list_def:
         list_definitions(args.list_def)
+    elif args.translate:
+        kwargs = {}
+        if getattr(args, 'from'):
+            kwargs['source'] = getattr(args, 'from')
+        if args.to:
+            kwargs['dest'] = args.to
+        translate(args.translate, **kwargs)
---
+++ core/translate.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import sys
+
+from googletrans import Translator
+
+def translate(*args, **kwargs):
+    text = ''
+    # Another way to do this is like:
+    # source = 'en'
+    # try:
+    #     source = kwargs['source']
+    # except KeyError:
+    #     pass
+    source = 'en' if 'source' not in kwargs else kwargs['source']
+    dest = 'es' if 'dest' not in kwargs else kwargs['dest']
+    if not args:
+        source = input('Enter the source language: ')
+        dest = input('Enter the destination language: ')
+        text = input('Enter a text to translate: ')
+    else:
+        text = ' '.join(args)
+    translator = Translator()
+    print(translator.translate(text, dest=dest, source=source).text)
+
+if __name__ == '__main__':
+    translate(sys.argv[1:])
